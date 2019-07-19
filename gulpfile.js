6d3b87621fe497bc05d5e2ae070b4d2454ad9111
Roni Laukkarinen <roni@laukkarinen.info>
2019-07-20 00:38:13 +0300

    Release dark version

--- gulpfile.js
+++ gulpfile.js
@@ -24,8 +24,6 @@ FILE PATHS
 ==========
 */
 var sassSrc = '*.{sass,scss}';
-var sassFile = 'theme.scss';
-var cssDest = './';
 
 /*
 
@@ -49,7 +47,7 @@ STYLES
 */
 
 gulp.task('styles', function() {
-  gulp.src(sassFile)
+  gulp.src('theme.scss')
     .pipe(sass({
       compass: false,
       bundleExec: true,
@@ -81,7 +79,41 @@ gulp.task('styles', function() {
         console.log('[clean-css] Time spent on minification: ' + details.stats.timeSpent + ' ms');
         console.log('[clean-css] Compression efficiency: ' + details.stats.efficiency * 100 + ' %');
     }))
-    .pipe(gulp.dest(cssDest))
+    .pipe(gulp.dest('./'))
+
+  gulp.src('theme-dark.scss')
+    .pipe(sass({
+      compass: false,
+      bundleExec: true,
+      sourcemap: false,
+      style: 'compressed',
+      debugInfo: true,
+      lineNumbers: true,
+      errLogToConsole: true,
+      includePaths: [
+        'node_modules/',
+        // 'bower_components/',
+        // require('node-bourbon').includePaths
+      ],
+    }))
+
+    .on('error', handleError('styles'))
+    .pipe(prefix('last 3 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')) // Adds browser prefixes (eg. -webkit, -moz, etc.)
+    .pipe(pixrem())
+    .pipe(minifycss({
+      advanced: true,
+      keepBreaks: false,
+      specialComments: 0,
+      mediaMerging: true,
+      sourceMap: true,
+      debug: true
+    }, function(details) {
+        console.log('[clean-css] Original size: ' + details.stats.originalSize + ' bytes');
+        console.log('[clean-css] Minified size: ' + details.stats.minifiedSize + ' bytes');
+        console.log('[clean-css] Time spent on minification: ' + details.stats.timeSpent + ' ms');
+        console.log('[clean-css] Compression efficiency: ' + details.stats.efficiency * 100 + ' %');
+    }))
+    .pipe(gulp.dest('./'))
 
 });
 
